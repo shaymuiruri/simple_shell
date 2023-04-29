3ba1c174221e284efe41774897a94446636962ed
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-04-28 22:37:20 -0700

    Shell loop c file

--- shell_loop.c
+++ shell_loop.c
@@ -1,32 +1,77 @@
 #include "main.h"
+
 /**
-* loop - function to get user input, tokenize directory and
-* check for builtins. Main loop to initiate shell
-* @env: A double pointer to an array of environment variables.
-* Return: void
-*/
-void loop(char **env)
+ * without_comment - deletes comments from the input
+ *
+ * @in: input string
+ * Return: input without comments
+ */
+char *without_comment(char *in)
 {
-char *line;
-char **dir, **command;
-char *combine;
+	int i, up_to;
 
-while (1)
-{
-prompt();
-signal(SIGINT, handler);
-line = get_line();
+	up_to = 0;
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
 
-command = split_line(line);
-dir = dirTok(env);
-combine = checkPath(dir, command[0]);
-checkBuiltins(combine, command);
-if (!combine)
-perror(combine);
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i;
+		}
+	}
+
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+
+	return (in);
 }
-buffers1(NULL, NULL);
-buffers2(NULL, NULL);
-buffers3(NULL, NULL);
-buffers4(NULL, NULL);
-buffers5(NULL);
+
+/**
+ * shell_loop - Loop of shell
+ * @datash: data relevant (av, input, args)
+ *
+ * Return: no return.
+ */
+void shell_loop(data_shell *datash)
+{
+	int loop, i_eof;
+	char *input;
+
+	loop = 1;
+	while (loop == 1)
+	{
+		write(STDIN_FILENO, "^-^ ", 4);
+		input = read_line(&i_eof);
+		if (i_eof != -1)
+		{
+			input = without_comment(input);
+			if (input == NULL)
+				continue;
+
+			if (check_syntax_error(datash, input) == 1)
+			{
+				datash->status = 2;
+				free(input);
+				continue;
+			}
+			input = rep_var(input, datash);
+			loop = split_commands(datash, input);
+			datash->counter += 1;
+			free(input);
+		}
+		else
+		{
+			loop = 0;
+			free(input);
+		}
+	}
 }
